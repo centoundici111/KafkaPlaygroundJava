79b23daeda3ca8324441cf03b1f33c4f7f0a2d41
Pablo Correa <pcorrea@thoughtworks.com>
2020-02-12 17:03:49 -0500

    produces the record .get was missing after send

--- src/main/java/kafka/MyProducer.java
+++ src/main/java/kafka/MyProducer.java
@@ -1,31 +1,38 @@
 package kafka;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.streams.StreamsConfig;
 
 import java.util.Properties;
+import java.util.UUID;
 
 public class MyProducer {
     Properties kafkaProps = new Properties();
 
 
     public MyProducer() {
-        kafkaProps.put("bootstrap.servers","localhost:9092");
-        kafkaProps.put("key.serializer",
+        kafkaProps.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        kafkaProps.put(ProducerConfig.CLIENT_ID_CONFIG, "cliente1");
+        kafkaProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
                 "org.apache.kafka.common.serialization.StringSerializer");
-        kafkaProps.put("value.serializer",
+        kafkaProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
                 "org.apache.kafka.common.serialization.StringSerializer");
-        kafkaProps.put("acks", "all");
+        kafkaProps.put(ProducerConfig.ACKS_CONFIG, "all");
     }
 
 
-    public void produce(){
-        final KafkaProducer<String, String> producer =new KafkaProducer<>(kafkaProps);
+    public void produce() {
+        final KafkaProducer<String, String> producer = new KafkaProducer<>(kafkaProps);
         ProducerRecord<String, String> record =
-                new ProducerRecord<>("test", "Precision Products",
+                new ProducerRecord<>("test",
+                        UUID.randomUUID().toString(),
                         "France");
         try {
-            producer.send(record);
+            RecordMetadata recordMetadata = producer.send(record).get();
+            System.out.println(recordMetadata.offset());
         } catch (Exception e) {
             e.printStackTrace();
         }
